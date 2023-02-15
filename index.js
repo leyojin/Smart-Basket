7937c9a7fef512c86293ccc173bbf601a5d604ea
Leyojin Thomas <leyojinthomas36@gmail.com>
2023-02-15 11:27:13 +0530

    cors

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const productRouter = require("./routes/product");
 const userRouter = require("./routes/user");
+var cors = require("cors");
 const DB =
   "mongodb+srv://karthik:karthik123@cluster0.vie2q.mongodb.net/smart-basket?retryWrites=true&w=majority";
 const PORT = process.env.PORT | 8080;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(productRouter);
 app.use(userRouter);
