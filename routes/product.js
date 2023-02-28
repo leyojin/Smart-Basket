b42447842e97244b2de3deaded1f76957fa2a59c
Leyojin Thomas <leyojinthomas36@gmail.com>
2023-02-28 15:06:09 +0530

    update

--- routes/product.js
+++ routes/product.js
@@ -11,38 +11,38 @@ productRouter.get("/products", async (req, res) => {
   }
 });
 
-const server = http.createServer(async (req, res) => {
-  if (req.method === "POST") {
-    let body = "";
-    req.on("data", (chunk) => {
-      body += chunk.toString();
-    });
-    req.on("end", async () => {
-      const data = qs.parse(body);
-      const productName = data.dropdownMenu;
-      const quantity = data.textField;
+// const server = http.createServer(async (req, res) => {
+//   if (req.method === "POST") {
+//     let body = "";
+//     req.on("data", (chunk) => {
+//       body += chunk.toString();
+//     });
+//     req.on("end", async () => {
+//       const data = qs.parse(body);
+//       const productName = data.dropdownMenu;
+//       const quantity = data.textField;
 
-      // Find the shop in MongoDB and update the quantity of the specified product
-      try {
-        const shop = await Shop.findOneAndUpdate(
-          { "product.name": productName },
-          { $set: { "product.$.quantity": quantity } },
-          { new: true }
-        );
-        res.writeHead(200, { "Content-Type": "text/plain" });
-        res.end(
-          `Quantity for ${productName} in ${shop.name} has been updated to ${quantity}`
-        );
-      } catch (error) {
-        console.error(error);
-        res.writeHead(500, { "Content-Type": "text/plain" });
-        res.end("Error updating quantity");
-      }
-    });
-  } else {
-    res.writeHead(404, { "Content-Type": "text/plain" });
-    res.end("Not found");
-  }
-});
+//       // Find the shop in MongoDB and update the quantity of the specified product
+//       try {
+//         const shop = await Shop.findOneAndUpdate(
+//           { "product.name": productName },
+//           { $set: { "product.$.quantity": quantity } },
+//           { new: true }
+//         );
+//         res.writeHead(200, { "Content-Type": "text/plain" });
+//         res.end(
+//           `Quantity for ${productName} in ${shop.name} has been updated to ${quantity}`
+//         );
+//       } catch (error) {
+//         console.error(error);
+//         res.writeHead(500, { "Content-Type": "text/plain" });
+//         res.end("Error updating quantity");
+//       }
+//     });
+//   } else {
+//     res.writeHead(404, { "Content-Type": "text/plain" });
+//     res.end("Not found");
+//   }
+// });
 
 module.exports = productRouter;
